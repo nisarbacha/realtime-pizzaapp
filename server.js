59dd1907d805ac38e6c5b0fa5ace5efa56499e42
Nisar <nisar.a@mblhightech.com>
2022-06-16 17:35:34 +0300

    login and registeration pages ready

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const app = express()
 const path = require('path')
 const ejs = require('ejs')
 const expressLayout = require("express-ejs-layouts")
-app.get('/', (req, res)=> {
-    res.render('home')
-})
-
-
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
-app.use(expressLayout);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on new port ${PORT}`);
+});
+
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+app.use(expressLayout);
+
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res)=> {
+    res.render('home')
+})
+app.get('/cart',(req, res)=>{
+  res.render('customers/cart')
+})
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
 });
